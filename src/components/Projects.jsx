19bf5774cb76e805eaebe613f104d891c7971f2e
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2022-02-26 15:30:08 +0530

    projects-section completed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,32 +50,45 @@ const Projects = () => {
       )}
 
       <div className="grid grid-cols-3 gap-4 mt-4 md:grid-cols-2 sm:grid-cols-1">
-        {nodes && nodes.map((project) => {
-          return (
-            <div key={project.id} className=" p-4 bg-lightBlue flex flex-col justify-start shadow-md rounded-md hover:-translate-y-2 transition-transform duration-300">
-              <div className="flex justify-between items-center text-onHover mb-4">
-                <a target="_blank" rel="noreferrer" href={project.frontmatter.source_code}>
-                  {" "}
-                  <FiGithub fontSize={30} />
-                </a>
-                <a target="_blank" rel="noreferrer" href={project.frontmatter.live_site}>
-                  {" "}
-                  <FiExternalLink fontSize={30} />
-                </a>
+        {nodes &&
+          nodes.map(project => {
+            return (
+              <div
+                key={project.id}
+                className=" p-4 bg-lightBlue flex flex-col justify-start shadow-md rounded-md hover:-translate-y-2 transition-transform duration-300"
+              >
+                <div className="flex justify-between items-center text-onHover mb-4">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={project.frontmatter.source_code}
+                  >
+                    {" "}
+                    <FiGithub fontSize={30} />
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={project.frontmatter.live_site}
+                  >
+                    {" "}
+                    <FiExternalLink fontSize={30} />
+                  </a>
+                </div>
+                <h2 className="font-semibold text-secondary text-xl tracking-wider hover:text-onHover transition-colors duration-300">
+                  {project.frontmatter.title}
+                </h2>
+
+                <div
+                  className="text-primary font-semibold mt-4 mb-4 text-justify"
+                  dangerouslySetInnerHTML={{ __html: project.html }}
+                />
+                <p className="text-primary mt-2 text-sm mt-auto">
+                  {project.frontmatter.stack}
+                </p>
               </div>
-              <h2 className="font-semibold text-secondary text-xl tracking-wider hover:text-onHover transition-colors duration-300">
-                {project.frontmatter.title}
-              </h2>
-              {/* <p className="text-primary font-semibold mt-4 mb-4 text-justify">
-                {project.project_description}
-              </p> */}
-              <div className="text-primary font-semibold mt-4 mb-4 text-justify" dangerouslySetInnerHTML={{__html:project.html}} />
-              <p className="text-primary mt-2 text-sm mt-auto">
-                {project.frontmatter.stack}
-              </p>
-            </div>
-          )
-        })}
+            )
+          })}
       </div>
     </section>
   )
